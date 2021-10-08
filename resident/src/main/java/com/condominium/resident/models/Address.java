4bac090311136360ba8ae5da26e136e4d3da1327
tiagosantos-dev <tiagoltj@hotmail.com>
2021-10-07 22:58:32 -0300

    Structuring address and resident class

--- resident/src/main/java/com/condominium/resident/models/Address.java
+++ resident/src/main/java/com/condominium/resident/models/Address.java
@@ -1,5 +1,51 @@
 package com.condominium.resident.models;
 
-public class Address {
+import java.io.Serializable;
+import java.util.UUID;
 
+public class Address implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private UUID id;
+	private String street;
+	private String number;
+	private String zipCode;
+	private String referencePoint;
+	
+	public Address() {}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getReferencePoint() {
+		return referencePoint;
+	}
+	public void setReferencePoint(String referencePoint) {
+		this.referencePoint = referencePoint;
+	}
+	
+	
+	
 }
--- resident/src/main/java/com/condominium/resident/models/Resident.java
+++ resident/src/main/java/com/condominium/resident/models/Resident.java
@@ -1,5 +1,44 @@
 package com.condominium.resident.models;
 
-public class Resident {
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Resident implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private UUID id;
+	
+	private Person person;
+	
+	private Point point;
+	
+	public Resident(){}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+	
+	
 
 }

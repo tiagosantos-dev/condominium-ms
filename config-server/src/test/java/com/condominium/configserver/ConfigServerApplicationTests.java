20fba09dc20ac4fe644e8b046b8594cffc628410
tiagosantos-dev <tiagoltj@hotmail.com>
2021-10-16 21:49:56 -0300

    Add config-server

---
+++ config-server/src/test/java/com/condominium/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condominium.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
